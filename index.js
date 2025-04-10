711d94ab7c3c1d658a2c8d3187a0f9f559cd1990
RedsGames <105242416+RedsGames@users.noreply.github.com>
2025-04-10 23:37:24 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+// pages/index.js
+
+export default function Home() {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '50px' }}>
+      <h1>Привет, Next.js!</h1>
+      <p>Это простое приложение, работающая на Next.js под Node.js 22.</p>
+    </div>
+  );
+}
